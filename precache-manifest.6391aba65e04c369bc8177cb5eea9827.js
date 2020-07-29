914576dd9ae5fbc3f4ce42bd48f24c3be1d57b04
@itsAj <ajaymati1000@gmail.com>
2020-07-29 11:39:06 +0530

    Updates

---
+++ precache-manifest.6391aba65e04c369bc8177cb5eea9827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a5639410e9edd5148bf62d09fd5ae6",
+    "url": "/seller/index.html"
+  },
+  {
+    "revision": "fed4458cf7b32e2087e0",
+    "url": "/seller/static/css/main.fa041564.chunk.css"
+  },
+  {
+    "revision": "0c59c0253d5171c7a83a",
+    "url": "/seller/static/js/2.25700eb6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/seller/static/js/2.25700eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed4458cf7b32e2087e0",
+    "url": "/seller/static/js/main.7f39be43.chunk.js"
+  },
+  {
+    "revision": "2b05bc2c2f17b15fe4c2",
+    "url": "/seller/static/js/runtime-main.278e8fd6.js"
+  }
+]);
