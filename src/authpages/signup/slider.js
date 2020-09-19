399fb348b059c0f5ff45d74211c29735b1affbd4
itsAj <ajaymati1000@gmail.com>
2020-09-19 14:47:23 +0530

    .

--- src/authpages/signup/slider.js
+++ src/authpages/signup/slider.js
@@ -10,7 +10,7 @@ import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-import BInfo from './Binfo';
+import BInfo from './BInfo';
 import UserInfo from './UserInfo';
 import Review from './Review';
 
